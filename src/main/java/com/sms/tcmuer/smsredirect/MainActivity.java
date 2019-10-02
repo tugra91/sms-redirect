56d17c2c356ecd2eaac833863d876359ed2064d6
tugra91 <37430988+tugra91@users.noreply.github.com>
2019-10-02 07:38:27 +0300

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/sms/tcmuer/smsredirect/MainActivity.java
@@ -0,0 +1,101 @@
+package com.sms.tcmuer.smsredirect;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[] { Manifest.permission.SEND_SMS}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[] { Manifest.permission.INTERNET}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[] { Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_BOOT_COMPLETED)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[] { Manifest.permission.RECEIVE_BOOT_COMPLETED}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[] { Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[] { Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[]
+            permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 10) {
+            Log.d("TAG", "My permission request sms received successfully");
+        }
+    }
+}
---
+++ src/main/java/com/sms/tcmuer/smsredirect/MyWakefulReceiver.java
@@ -0,0 +1,192 @@
+package com.sms.tcmuer.smsredirect;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class MyWakefulReceiver extends WakefulBroadcastReceiver {
+
+
+    private static final String TAG = "SMSBroadcastReceiver";
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Geldi geldi");
+
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs;
+        String strMessage = "";
+
+        Object[] pdus = (Object[]) bundle.get("pdus");
+
+        if (pdus != null) {
+
+            msgs = new SmsMessage[pdus.length];
+
+            for (int i = 0; i < msgs.length; i++) {
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                strMessage += "\"" + msgs[i].getMessageBody() + "\"" + "\n";
+
+                fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+
+
+                LocationRequest request = new LocationRequest();
+                request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                request.setExpirationDuration(10000l);
+                request.setExpirationTime(10000l);
+
+
+                LocationResult locationResult = new LocationResult()
+
+                LocationCallback locationCallback = new LocationCallback();
+                locationCallback.onLocationResult();
+
+                fusedLocationClient.requestLocationUpdates(request, )
+
+
+
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                fusedLocationClient.getLastLocation();
+                Thread mailThread = new Thread(new SendMail(msgs[i].getDisplayOriginatingAddress(), strMessage, context));
+                mailThread.start();
+
+
+                Log.i(TAG, strMessage);
+
+
+
+            }
+        }
+    }
+
+
+
+    private class SendMail implements  Runnable {
+
+
+        private String smsSender;
+        private String smsMessage;
+        private Context context;
+
+        public SendMail(String smsSender, String smsMessage, Context context) {
+            this.smsSender = smsSender;
+            this.smsMessage = smsMessage;
+            this.context = context;
+        }
+
+
+        public void run() {
+            sendMail();
+        }
+
+        private void sendMail() {
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+            final String username = "fromMail@gmail.com";
+            final String password = "sifre";
+
+            Properties prop = new Properties();
+            prop.put("mail.smtp.host", "smtp.gmail.com");
+            //prop.put("mail.smtp.port", "587");
+            prop.put("mail.smtp.connectiontimeout", "10000");
+            prop.put("mail.smtp.timeout", "10000");
+            prop.put("mail.smtp.port", "465");
+            prop.put("mail.smtp.auth", "true");
+            prop.put("mail.smtp.socketFactory.port", "465");
+            prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            //prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+            Session session = Session.getInstance(prop,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("SMS Yönlendirme")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+
+
+            try {
+
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("fromamail@mail.com","SMS Yönlendirme"));
+                message.setRecipients(
+                        Message.RecipientType.TO,
+                        InternetAddress.parse("toMail@mail.com")
+                );
+                message.setSubject(smsSender + " SMS Geldi");
+                message.setText("Sevgili Tuğra " + smsSender + " ' dan SMS geldi "
+                        + "\n\n " + smsMessage);
+
+
+                Transport.send(message);
+
+                smsMessage += "Mail olarak mesaj Gönderildi " + smsMessage;
+
+                builder.setContentText(smsMessage)
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(smsMessage));
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage("Redirect SMS Number",null,smsMessage,null,null);
+                smsMessage += "Mail Hata Aldı SMS olarak mesaj Gönderildi " + smsMessage;
+                builder.setContentText(smsMessage)
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(smsMessage));
+            } finally {
+                Date now = new Date();
+                int id = Integer.parseInt(new SimpleDateFormat("ddHHmmss",  Locale.US).format(now));
+
+                notificationManager.notify(id, builder.build());
+            }
+        }
+    }
+
+
+
+
+}
